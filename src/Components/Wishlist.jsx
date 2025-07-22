ea5a9072d1cc136cfcf131fddef4f533545a6fbd
Mohammed Salih k <salihsha656@gmail.com>
2025-07-22 20:48:21 +0530

    updatedd

--- src/Components/Wishlist.jsx
+++ src/Components/Wishlist.jsx
@@ -14,7 +14,7 @@ function Wishlist() {
   };
 
   return (
-    <div className="p-4 md:p-6 container mx-auto max-w-7xl h-screen">
+    <div className="p-4 md:p-6 container mx-auto max-w-7xl ">
       <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6">
         {savedProducts.length === 0 ? (
           <p className="col-span-full text-center text-gray-500">No items in Save</p>
